1010055bba648387c36d426b439251f48b02d490
DhoriRohit1 <rohitdhori3@gmail.com>
2024-10-05 09:58:03 +0530

    first commit

---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from "react-router-dom";
+import "./Header.scss";
+import { MdFoodBank} from "react-icons/md";
+import { useSidebarContext } from '../../context/sidebarContext';
+
+const Navbar = () => {
+  const {openSidebar} = useSidebarContext();
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if(offset > 60){
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  })
+
+  return (
+    <nav className={`navbar bg-orange flex align-center ${scrolled ? 'scrolled': ""}`}>
+      <div className='container w-100'>
+        <div className='navbar-content text-white'>
+          <div className='brand-and-toggler flex align-center justify-between'>
+            <Link to = "/" className='navbar-brand fw-3 fs-22 flex align-center'>
+              <MdFoodBank />
+              <span className='navbar-brand-text fw-7'>Recipe App.</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
